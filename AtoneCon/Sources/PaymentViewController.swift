02edc82f3c3941dd9c0bc7270ca228ff38c41111
Su Ho V <su.ho@asiantech.vn>
2017-07-04 09:31:23 +0700

    - inject javascript - load file html from resources

--- AtoneCon/Sources/PaymentViewController.swift
+++ AtoneCon/Sources/PaymentViewController.swift
@@ -13,19 +13,6 @@ final internal class PaymentViewController: UIViewController {
 
     private var webView: WKWebView!
 
-    private var htmlContent: String {
-        let bundle = Bundle(for: PaymentViewController.self)
-        guard let url = bundle.url(forResource: "atone", withExtension: "html") else {
-            fatalError("File Not Found")
-        }
-        do {
-            let string = try String(contentsOf: url)
-            return string
-        } catch {
-            fatalError("File Not Found")
-        }
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setupWebView()
@@ -34,6 +21,41 @@ final internal class PaymentViewController: UIViewController {
     private func setupWebView() {
         webView = WKWebView(frame: view.bounds)
         view.addSubview(webView)
-        webView.loadHTMLString(htmlContent, baseURL: nil)
+        webView.loadHTMLString(htmlContent(), baseURL: nil)
+        injectScripts()
+    }
+
+    private func injectScripts() {
+        let controller = webView.configuration.userContentController
+        controller.addUserScript(userScript())
+    }
+
+    private func htmlContent() -> String {
+        do {
+            let url = self.url(forResource: "atone", withExtension: "html")
+            let string = try String(contentsOf: url)
+            return string
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+    private func userScript() -> WKUserScript {
+        do {
+            let url = self.url(forResource: "atone", withExtension: "js")
+            let source = try String(contentsOf: url, encoding: .utf8)
+            let userScript = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+            return userScript
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+    private func url(forResource name: String?, withExtension ext: String?) -> URL {
+        let bundle = Bundle(for: PaymentViewController.self)
+        guard let url = bundle.url(forResource: name, withExtension: ext) else {
+            fatalError("File Not Found")
+        }
+        return url
     }
 }
