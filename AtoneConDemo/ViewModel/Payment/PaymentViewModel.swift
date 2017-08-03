bd1e4d2ae5301879a5a209dee7970c205293f04e
Hanh Pham N <hanh.pham@asiantech.vn>
2017-08-03 16:51:14 +0700

    format code

--- AtoneConDemo/ViewModel/Payment/PaymentViewModel.swift
+++ AtoneConDemo/ViewModel/Payment/PaymentViewModel.swift
@@ -34,7 +34,7 @@ final class PaymentViewModel {
         customer.address = "東京都中央区銀座１－１０ー６　銀座ファーストビル４階"
         customer.tel = "080-1234-1234"
         customer.email = "np@netprotections.co.jp"
-        customer.totalPurchaseAmount = 20000
+        customer.totalPurchaseAmount = 20_000
         customer.totalPurchaseCount = 2
         return customer
     }
